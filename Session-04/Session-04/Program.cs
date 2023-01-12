38b320eb7fb40cb38ef6cd64687f0d3445447a86
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-12 15:26:50 +0200

    task 1 and 2

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //task 1
+        Console.WriteLine("Hello Vlasis");
+
+        //task 2
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+     console.WriteLine(sum);
+    }
+}
