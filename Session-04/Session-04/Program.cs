d14ba72df7cc52fa7752ed510f89e9567eccbdc6
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-12 18:16:14 +0200

    task 3

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -4,7 +4,18 @@ Console.WriteLine("Hello Vlasis");
 //task 2
 int a2 = 1;
 int b2 = 2;
-int sum2 = a + b;
+int sum2 = a2 + b2;
 Console.WriteLine(sum2);
 
 //task 3
+
+int a3 = -1 + 5 * 6;
+Console.WriteLine(a3);
+int b3 = 38 + 5 % 7;
+Console.WriteLine(b3);
+int c3 = 14 + (-3 * 6) / (7);
+Console.WriteLine(c3);
+int d3 = 2 + 13 / 6 * 6 + (int)Math.Sqrt(7);
+Console.WriteLine(d3);
+int e3 = (int)(Math.Pow(6, 4) + Math.Pow(5, 7)) /(9 % 4);
+Console.WriteLine(e3);
