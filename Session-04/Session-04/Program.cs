72dbeb3e0be0db3534a1d01ae5bdf76410ee3026
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-12 18:01:36 +0200

    now it runs!!

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -1,14 +1,8 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        //task 1
-        Console.WriteLine("Hello Vlasis");
+﻿//task 1
+Console.WriteLine("Hello Vlasis");
 
-        //task 2
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
-     console.WriteLine(sum);
-    }
-}
+//task 2
+int a = 1;
+int b = 2;
+int sum = a + b;
+Console.WriteLine(sum);
