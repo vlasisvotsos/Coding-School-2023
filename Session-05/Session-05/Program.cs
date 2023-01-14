5c283a10f28c24a43304d69ac20bccfd3c5adec2
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-14 13:56:35 +0200

    helping array created

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -4,9 +4,13 @@ string name = "vlasis";
 // Console.WriteLine(name);
 
 char[] charArray = new char[name.Length];
+char[] help = new char[name.Length];
 for (int i = 0; i < charArray.Length; i++)
 {
     charArray[i] = name[i];
+    help[i] = name[i];
     Console.WriteLine(charArray[i]);
 }
+
+Console.ReadLine();
 
