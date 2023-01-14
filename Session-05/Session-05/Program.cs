1ba5fed06f3e74e6054c396f638190fb3cb9b458
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-14 13:36:49 +0200

    Session-05 created

---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+char[] name = new char[6];
+name[0] = 'v';
+name[1] = 'l';
+name[2] = 'a';
+name[3] = 's';
+name[4] = 'i';
+name[5] = 's';
+
+for (int i = 5; i >=0;i--)
+    {
+    Console.WriteLine(name[i]);
+    }
+
