8ef90d1089452013b513562d3764d3afbee2686e
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-14 13:46:19 +0200

    string converted  to char array

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -3,3 +3,10 @@
 string name = "vlasis";
 // Console.WriteLine(name);
 
+char[] charArray = new char[name.Length];
+for (int i = 0; i < charArray.Length; i++)
+{
+    charArray[i] = name[i];
+    Console.WriteLine(charArray[i]);
+}
+
