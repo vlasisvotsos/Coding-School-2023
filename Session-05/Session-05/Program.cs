4a53b67629bcc1570aecb8da670840b5d94949d9
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-14 16:17:07 +0200

    task 2

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -13,14 +13,32 @@ for (int i=0;i< charArray.Length; i++)
     charArray[i] = help[charArray.Length - i -1];
 }
    string reversedName = new string(charArray);
-string answer = string.Format("my name is {0} and my name reversed is {1}",name,reversedName);
-Console.WriteLine(answer);
+string answer1 = string.Format("my name is {0} and my name reversed is {1}",name,reversedName);
+Console.WriteLine(answer1);
 
 // task 2
 
+int answer2 = 0;
 Console.WriteLine("Please insert an integer");
-int j = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Please enter s to calculate the sum from 1 to selected number or p for the product");
-//char c = Convert.ToChar(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter s to calculate the sum from 1 to selected number or p for the product");
+string c2 = Console.ReadLine();
+if(c2 == "s" )
+{
+    for(int i = 0; i <= num2; i++)
+    {
+        answer2 += i;
+    }
+    String.Format("The sum from 1 to {0} is {1}",num2,answer2);
+}
+if(c2=="p")
+{
+    for (int i = 0; i <= num2; i++)
+    {
+        answer2 = answer2 * i;
+    }
+    String.Format("The product from 1 to {0} is {1}", num2, answer2);
+}
+
 
 
