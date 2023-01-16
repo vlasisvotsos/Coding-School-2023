a269c072765c73f04a2395b0743495a7cfd61d41
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 14:46:17 +0200

    classes created

---
+++ Session-06/Session-06/Professor.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Professor : Person
+{
+	public Professor()
+	{
+	}
+}
---
+++ Session-06/Session-06/Student.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Student : Person
+{
+	public Student()
+	{
+	}
+}
