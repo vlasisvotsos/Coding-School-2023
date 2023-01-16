a4416cf984cea47bf86d20718e30eab083241df0
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 16:52:32 +0200

    course created

--- Session-06/Session-06/Course.cs
+++ Session-06/Session-06/Course.cs
@@ -3,9 +3,27 @@ namespace Session-06
 {
 	public class Course
 	{
-	public Course()
-	{
+		public Guid ID { get; set; }
+		public string Code { get; set; }
+		public string Subject { get; set; }
+		public Course()
+		{
 
-	}
-	}
+		}
+		public Course(Guid id)
+		{
+			ID= id;
+		}
+		public Course(Guid id,string code)
+		{
+			ID = id;
+			Code = code;
+		}
+		public Course(Guid id, string code, string subject)
+		{
+			ID = id;
+			Code = code;
+			Subject = subject;
+		}
+}
 }
