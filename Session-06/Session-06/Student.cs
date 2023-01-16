3f144777b42e975da97816ca32b89545aa9c7076
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 15:51:19 +0200

    student completed

--- Session-06/Session-06/Student.cs
+++ Session-06/Session-06/Student.cs
@@ -2,7 +2,36 @@
 
 public class Student : Person
 {
+	public int RegistrationNumber { get; set; }
+	public Course Course[] { get; set; }
 	public Student()
 	{
+	}
+	public Student(Guid id) : base (id)
+	{
+	}
+	public Student(Guid id,string name) : base(id,name)
+    {
+	}
+	public Student(Guid id, string name, int age) : base(id, name, age)
+    {
+	}
+	public Student(Guid id, string name, int age,int registrationNumber) : base(id, name, age)
+    {
+	RegistrationNumber = registrationNumber;
+	}
+	public Student(Guid id, string name, int age, int registrationNumber,Course course[]) : base(id, name, age)
+    {
+    RegistrationNumber = registrationNumber;
+	Course[]= course[];
+
+	}
+	public void Attend(Corse corse, DateTime date)
+	{
+
+	}
+	public void WriteExam(Corse corse, DateTime date)
+	{
+
 	}
 }
