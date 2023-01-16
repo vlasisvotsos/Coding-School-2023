b109f08273e572cd9212a7a0862aa9edc0ff7267
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 15:00:44 +0200

    Person Class completed

--- Session-06/Session-06/Person.cs
+++ Session-06/Session-06/Person.cs
@@ -2,8 +2,34 @@
 
 public class Person
 {
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+	public int Age { get; set; }
 	public Person()
 	{
 
+	}
+	public Person(Guid id)
+	{
+		Id= id;
+	}
+	public Person(Guid id,string name)
+	{
+		Id= id;
+		Name= name;
+	}
+	public Person(Guid id, string name,int age)
+	{
+		Id= id;
+		Name= name;
+		Age= age;
+	}
+	public void GetName()
+	{
+
+	}
+	public void SetName(string name)
+	{
+		Name= name;
 	}
 }
