8775880262607f18e1348d1a04b84730ff61d058
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 15:56:46 +0200

    institute created

--- Session-06/Session-06/Institute.cs
+++ Session-06/Session-06/Institute.cs
@@ -2,7 +2,33 @@
 
 public class Institute
 {
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+	public int YearsInService { get; set; }
 	public Institute()
+	{
+
+	}
+    public Institute(Guid id)
+    {
+		Id= id;
+    }
+    public Institute(Guid id, string name)
+    {
+        Id = id;
+		Name= name;
+    }
+    public Institute(Guid id, string name,int yearsInService)
+    {
+        Id = id;
+        Name = name;
+        YearsInService = yearsInService;
+    }
+    public void GetName()
+	{
+
+	}
+	public void SetName(string name)
 	{
 
 	}
