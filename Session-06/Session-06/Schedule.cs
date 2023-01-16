8efb077d0fe84f85111cad0b377d197cff7619b9
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 17:01:46 +0200

    Schedule created

--- Session-06/Session-06/Schedule.cs
+++ Session-06/Session-06/Schedule.cs
@@ -4,9 +4,35 @@ namespace Session-06
 {
 	public class Schedule
 	{
+		public Guid Id { get; set; }
+		public Guid CourseID { get; set; }
+		public Guid ProfessorID { get; set; }
+		public DateTime Callender { get; set; }
 		public Schedule()
 		{
 
+		}
+		public Schedule(Guid id)
+		{
+			Id = id;
+		}
+		public Schedule(Guid id,Guid courseID)
+		{
+			Id = id;
+			CourseID = courseID;
+		}
+		public Schedule(Guid id, Guid courseID,Guid professorID)
+		{
+			Id = id;
+			CourseID = courseID;
+			ProfessorID= professorID;
+		}
+		public Schedule(Guid id, Guid courseID, Guid professorID,DateTime callender)
+		{
+			Id = id;
+			CourseID = courseID;
+			ProfessorID = professorID;
+			Callender = callender;
 		}
 	}
 }
