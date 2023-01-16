a918b04e5c02387e4ed004e2aa6873edb136cc1b
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-16 16:48:52 +0200

    grade created

--- Session-06/Session-06/Grade.cs
+++ Session-06/Session-06/Grade.cs
@@ -5,9 +5,35 @@ namespace Session-06
 
 	public class Grade
 	{
-	public Grade()
-	{
+		public Guid ID { get; set; }
+		public Guid StudentID { get; set; }	
+		public Guid CourseID { get; set; }
+		public int Grade { get; set; }
+		public Grade()
+		{
 
-	}
-	}
+		}
+		public Grade(Guid id)
+		{
+			ID= id;
+		}
+		public Grade(Guid id,Guid studentID)
+		{
+			ID = id;
+			StudentID= studentID;
+		}
+		public Grade(Guid id, Guid studentID,Guid courseID)
+		{
+			ID = id;
+			StudentID = studentID;
+			CourseID= courseID;
+		}
+		public Grade(Guid id, Guid studentID, Guid courseID,int grade)
+		{
+			ID = id;
+			StudentID = studentID;
+			CourseID = courseID;
+			Grade = grade;
+		}
+}
 }
