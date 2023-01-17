5a5be7133070d254d28421260e5627afefda8ac7
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-17 17:22:59 +0200

    program cs changed

--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Session_07;
+
+ActionRequest request = new ActionRequest();
+ActionResponse response = new ActionResponse();
+
+ActionResolver resolver = new ActionResolver();
+
+response = resolver.Execute(request);
+
