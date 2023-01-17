5281596d7694023b632b2861d32f1960e1c90667
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-17 17:24:47 +0200

    b

--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -3,8 +3,6 @@ using Session_07;
 
 ActionRequest request = new ActionRequest();
 ActionResponse response = new ActionResponse();
-
 ActionResolver resolver = new ActionResolver();
-
 response = resolver.Execute(request);
 
