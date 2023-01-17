d70c5bfc7a12b80ee68dca0e13315712b5fd0060
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-17 21:15:25 +0200

    fix errors

--- Session-07/Session-07/ActionRequest.cs
+++ Session-07/Session-07/ActionRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,19 @@ namespace Session_07
         public ActionRequest(Guid requestID,ActionEnums action)
         {
             RequestID= requestID;
-            action = ActionEnums.Convertion;
+            switch (action)
+            {
+                case ActionEnums.Convertion:
+                    break;
+                case ActionEnums.Uppercase:
+                    break;
+                case ActionEnums.Reverse:
+                    break;
+                default:
+                    Console.WriteLine("Error Message");
+                    break;
+
+            }
         }
 
         public ActionRequest(Guid requestID)
