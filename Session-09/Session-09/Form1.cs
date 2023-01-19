a1bdca69fad2ac6db8c9ef151cde668f9947e579
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-19 16:10:55 +0200

    decimals created

--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -2,6 +2,9 @@ namespace Session_09
 {
     public partial class Form1 : Form
     {
+        private decimal? _value1 = null;
+        private decimal? _value2 = null;
+        private decimal? _result = null;
         enum Operation
         {
             Addition,
