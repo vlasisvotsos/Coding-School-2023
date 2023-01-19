45a874b57c192b10f7e26c1af02c041c6a54e9ce
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-19 16:13:42 +0200

    text read only

--- Session-09/Session-09/Form1.Designer.cs
+++ Session-09/Session-09/Form1.Designer.cs
@@ -213,6 +213,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(33, 137);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(745, 35);
             this.textBox1.TabIndex = 16;
             this.textBox1.Text = "0";
