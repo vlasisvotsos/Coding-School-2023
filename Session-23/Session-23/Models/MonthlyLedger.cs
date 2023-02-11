7a2ad0ee6bf16e77304970d928548a4377619a6a
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-11 21:44:08 +0200

    model added

---
+++ Session-23/Session-23/Models/MonthlyLedger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Model
+{
+    public class MonthlyLedger
+    {
+        DateTime Year { get; set; }
+        DateTime Month { get; set; }
+        decimal Income { get; set; }
+        decimal Expenses { get; set; }
+        decimal Total { get; set; }
+       
+        public MonthlyLedger(DateTime year,DateTime month,decimal income,decimal expenses,decimal total) 
+        { 
+            Year= year;
+            Month= month;
+            Income= income;
+            Expenses= expenses;
+            Total= total;
+        }
+
+    }
+}
