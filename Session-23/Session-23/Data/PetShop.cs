818e1840c80ade49b6fb1b9baddd3316d73a7ce5
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-11 22:13:18 +0200

    3

--- Session-23/Session-23/Data/PetShop.cs
+++ Session-23/Session-23/Data/PetShop.cs
@@ -5,11 +5,11 @@ namespace Session_23
 {
     public class PetShop : DbContext
     {
-        public DbSet<Pet> Pets { get; set; } = null;
-        public DbSet<Customer> Customers { get; set; } = null;
-        public DbSet<Employee> Employees { get; set; } = null;
-        public DbSet<MonthlyLedger> MonthlyLedger { get; set; } = null;
-        public DbSet<PetFood> PetFood { get; set; } = null;
-        public DbSet<Transaction> Transaction { get; set; } = null;
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<MonthlyLedger> MonthlyLedger { get; set; }
+        public DbSet<PetFood> PetFood { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
     }
 }
