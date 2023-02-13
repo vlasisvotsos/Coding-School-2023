d9c629bf1254eb8ce447ad1686a4c7d8e4952572
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-13 17:36:41 +0200

    PetFood page created

--- Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
+++ Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
@@ -34,6 +34,10 @@ namespace PetShop.MVC.Controllers
         {
             return View();
         }
+        public IActionResult PetFood()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
