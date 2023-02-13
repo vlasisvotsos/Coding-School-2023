03739efdb88177e9f962b964b8e64ec98d2add0e
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-13 17:20:02 +0200

    Pet page created

--- Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
+++ Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace PetShop.MVC.Controllers
         {
             return View();
         }
+        public IActionResult Pet()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
