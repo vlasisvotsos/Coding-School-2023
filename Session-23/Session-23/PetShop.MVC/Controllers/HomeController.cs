6229b74070033445babe991d911e4fcb03b5eeab
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-13 17:42:25 +0200

    transaction

--- Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
+++ Session-23/Session-23/PetShop.MVC/Controllers/HomeController.cs
@@ -38,6 +38,10 @@ namespace PetShop.MVC.Controllers
         {
             return View();
         }
+        public IActionResult Transaction()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
