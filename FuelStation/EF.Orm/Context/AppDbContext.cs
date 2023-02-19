de376e2e9a21805d7e4c35a999106473d9691296
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-19 14:44:07 +0200

    Context created

---
+++ FuelStation/EF.Orm/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Orm.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get;set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionLine> TransactionsLines { get; set; }
+        public DbSet<Ledger> Ledgers { get; set;}
+
+
+    }
+}
