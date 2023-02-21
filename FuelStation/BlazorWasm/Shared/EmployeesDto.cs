291780e2fa39292f734c9586f1e5c76ed5fa61c6
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-21 18:51:51 +0200

    dto created

---
+++ FuelStation/BlazorWasm/Shared/EmployeesDto.cs
@@ -0,0 +1,21 @@
+﻿using EF.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasm.Shared
+{
+    public class EmployeesDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime HiretDateStart { get; set; }
+        public DateTime HireDateEnd { get; set; }
+        public int SallaryPerMonth { get; set; }
+        public EmployeeTypeEnum EmployeeType { get; set; }
+    }
+}
---
+++ FuelStation/BlazorWasm/Shared/ItemsDto.cs
@@ -0,0 +1,20 @@
+﻿using EF.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasm.Shared
+{
+    public class ItemsDto
+    {
+        public int ID { get; set; }
+        public int Code { get; set; }
+        public string Description { get; set; }
+        public ItemTypeEnum ItemType { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
