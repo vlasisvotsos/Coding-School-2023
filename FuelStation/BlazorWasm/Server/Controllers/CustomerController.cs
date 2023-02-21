d51df88609bf48c9f45b13fa287e198b0efe9ff2
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-21 16:14:52 +0200

    a

---
+++ FuelStation/BlazorWasm/Server/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasm.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+       
+    }
+}
