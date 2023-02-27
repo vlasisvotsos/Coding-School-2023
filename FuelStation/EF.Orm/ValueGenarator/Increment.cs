8b0d5d1607a55b2aa94990ef4b57d13751dbb089
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-27 15:25:51 +0200

    value generator fix

--- FuelStation/EF.Orm/ValueGenarator/Increment.cs
+++ FuelStation/EF.Orm/ValueGenarator/Increment.cs
@@ -11,55 +11,16 @@ namespace EF.Orm.ValueGenarator
     {
         public static string AlphaNumericValue(string value)
         {
-
-            if (string.IsNullOrWhiteSpace(value))
+            string? result = null;
+            int num = 0;
+            for(int i=1; i <= value.Length -1; i++)
             {
-                return "";
+                result += value[i];
             }
-
-            if (Regex.IsMatch(value, "^[a-zA-Z0-9]+$") == false)
-            {
-                throw new Exception("Invalid Character: Must be a-Z or 0-9");
-            }
-
-            var characterArray = value.ToCharArray();
-
-            for (var charIndex = characterArray.Length - 1; charIndex >= 0; charIndex--)
-            {
-                var charValue = Convert.ToInt32(characterArray[charIndex]);
-
-                if (charValue != 57 && charValue != 90 && charValue != 122)
-                {
-
-                    characterArray[charIndex]++;
-
-                    for (int myResetIndex = charIndex + 1; myResetIndex < characterArray.Length; myResetIndex++)
-                    {
-
-                        charValue = Convert.ToInt32(characterArray[myResetIndex]);
-                        if (charValue >= 65 && charValue <= 90)
-                        {
-                            characterArray[myResetIndex] = 'A';
-                        }
-                        else if (charValue >= 97 && charValue <= 122)
-                        {
-                            characterArray[myResetIndex] = 'a';
-                        }
-                        else if (charValue >= 48 && charValue <= 57)
-                        {
-                            characterArray[myResetIndex] = '0';
-                        }
-
-                    }
-
-                    return new string(characterArray);
-
-                }
-
-            }
-
-                return null;
-
+            num = Convert.ToInt32(result);
+            num++;
+            result = "A" + num;
+            return result;
         }
     }
 }
